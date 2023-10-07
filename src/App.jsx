4c2b8e01bc0c67d63c916a36f31d10c17f78b35a
mishael-codes <enyimishael22@gmail.com>
2023-10-07 16:57:34 +0100

    added the delete function, minor UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,67 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(null);
   const style = {
     display: "flex",
-    width: "400px",
+    flexDirection: "column",
+    width: "60vw",
     border: "2px solid orange",
     padding: "15px",
     textAlign: "left",
     fontWeight: "500",
     borderRadius: "15px",
-    marginBottom: "15px"
-  }
+    marginBottom: "15px",
+  };
+
+  let url = "https://jsonplaceholder.typicode.com/todos";
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
+    fetch(url)
       .then((response) => response.json())
-      .then((data) => setData(data))
-
-  }, [])
+      .then((data) => setData(data));
+  }, []);
 
   const editText = (e) => {
-    let read = e.target.previousElementSibling
-    read.removeAttribute('readOnly');
-    read.focus()
-  }
+    let read = e.target.previousElementSibling;
+    read.removeAttribute("readOnly");
+    read.focus();
+  };
 
   const saveText = (e) => {
-    let read = e.target.previousElementSibling
-    let save = read.previousElementSibling
-    save.innerHTML = save.value
-    save.setAttribute('readOnly', true)
+    let read = e.target.previousElementSibling;
+    let save = read.previousElementSibling;
+    save.innerHTML = save.value;
+    save.setAttribute("readOnly", true);
     alert("You have edited a text");
     console.log(save);
-  }
+  };
 
+  const deleteText = (e) => {
+    let toBeDeleted = e.target.parentElement;
+    toBeDeleted.remove()
+  };
+  
   return (
     <>
       {data &&
         data.map((post) => {
-          return <div style={style}>
-            <textarea readOnly key={post.id}>{post.title}</textarea>
-            <button className="edit" onClick={editText}>Edit</button>
-            <button onClick={saveText} >Save</button>
-          </div>
+          return (
+            <div style={style}>
+              <textarea readOnly key={post.id}>
+                {post.title}
+              </textarea>
+              <button className="edit" onClick={editText}>
+                Edit
+              </button>
+              <button onClick={saveText}>Save</button>
+              <button className="delete" onClick={deleteText}>Delete</button>
+            </div>
+          );
         })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
